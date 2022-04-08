9633d1398bd9885508d00da9ad7137c0a59d9a7e
acpucio <acpucio@gmail.com>
2022-04-07 21:28:58 -0500

    Section 23: Hibernate Advanced Mappings - @OneTOne

--- src/com/luv2code/jdbc/TestJdbc.java
+++ src/com/luv2code/jdbc/TestJdbc.java
@@ -8,7 +8,7 @@ public class TestJdbc {
 	public static void main(String[] args) {
 
 		try {
-			String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?userSSL=false";
+			String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?userSSL=false";
 			String user = "hbstudent";
 			String pass = "HB_student_77099";
 			
