e3835aaef5925c3774595108f2a4106bee30d66a
acpucio <acpucio@gmail.com>
2022-04-10 00:36:59 -0500

    Section 25: Hibernate Advanced Mappings - Eager vs Lazy Loading

---
+++ src/com/luv2code/hibernate/demo/entity/EagerLazyDemo.java
@@ -0,0 +1,60 @@
+package com.luv2code.hibernate.demo.entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class EagerLazyDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory 
+		SessionFactory factory = new Configuration()
+										.configure("hibernate.cfg.xml")
+										.addAnnotatedClass(Instructor.class)
+										.addAnnotatedClass(InstructorDetail.class)
+										.addAnnotatedClass(Course.class)
+										.buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+
+			// start a transaction
+			session.beginTransaction();
+			
+			// get the instructor from db
+			int theId = 1;
+			Instructor tempInstructor = session.get(Instructor.class, theId);
+			
+			System.out.println("luv2code: Instructor: " + tempInstructor);
+			
+			System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
+						
+			// commit transaction	
+			session.getTransaction().commit();
+			
+			// close the session
+			session.close();
+			
+			System.out.println("\nluv2code: The sessis now closed!\n");
+			
+			// option 1: call getter method while session is open
+			
+			// since courses are lazy loaded ... this should fail
+			// get course for the instructor
+			System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
+			
+			System.out.println("luv2code: Done!");
+		}
+		finally {
+			
+			// add cleanup code
+			session.close();
+			
+			factory.close();
+		}
+		
+	}
+
+}
---
+++ src/com/luv2code/hibernate/demo/entity/FetchJoinDemo.java
@@ -0,0 +1,72 @@
+package com.luv2code.hibernate.demo.entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+public class FetchJoinDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory 
+		SessionFactory factory = new Configuration()
+										.configure("hibernate.cfg.xml")
+										.addAnnotatedClass(Instructor.class)
+										.addAnnotatedClass(InstructorDetail.class)
+										.addAnnotatedClass(Course.class)
+										.buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+
+			// start a transaction
+			session.beginTransaction();
+			
+			// option 2: Hibernate query with HQL
+			
+			// get the instructor from db
+			int theId = 1;
+
+			Query<Instructor> query = 
+					session.createQuery("select i from Instructor i "
+							+ "JOIN FETCH i.courses "
+							+ "where i.id=:theInstructorId", 
+							Instructor.class);
+			
+			// set parameter on query
+			query.setParameter("theInstructorId", theId);
+			
+			// execute query and get instructor
+			Instructor tempInstructor = query.getSingleResult();
+			
+			System.out.println("luv2code: Instructor: " + tempInstructor);
+			
+			// commit transaction	
+			session.getTransaction().commit();
+			
+			// close the session
+			session.close();
+			
+			System.out.println("\nluv2code: The sessis now closed!\n");
+			
+			// option 1: call getter method while session is open
+			
+			// since courses are lazy loaded ... this should fail
+			// get course for the instructor
+			System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
+			
+			System.out.println("luv2code: Done!");
+		}
+		finally {
+			
+			// add cleanup code
+			session.close();
+			
+			factory.close();
+		}
+		
+	}
+
+}
